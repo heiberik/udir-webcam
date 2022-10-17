3a9e1246494e16afc280f2ec9f0f00ca7d96e51b
heiberik <heiberik@hotmail.com>
2022-10-17 16:49:44 +0200

    uibuild

--- server/server.js
+++ server/server.js
@@ -2,16 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import { createServer } from "http"
 import { Server } from "socket.io"
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express()
 const httpServer = createServer()
 
+const io = new Server(httpServer, { cors: { origin: '*' } })
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
-const io = new Server(httpServer, { cors: { origin: '*' } })
+console.log("DIRNAME: ", __dirname);
+app.use(express.static(path.resolve(__dirname, "build")));
+
 
 io.on("connection", (socket) => {
 
