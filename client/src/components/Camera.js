61c277480d452ac5a0c19f28346cf173cbebaa33
Henrik Heiberg <heiberik@hotmail.com>
2022-10-27 09:46:01 +0200

    uibuild

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -144,7 +144,6 @@ const Camera = ({ withId }) => {
                 <CameraWidget 
                     isFullscreen={true}
                     idealFacingMode={FACING_MODES.ENVIRONMENT}
-                    imageCompression={0.4}
                     imageType={IMAGE_TYPES.JPG}
                     isMaxResolution={true}
                     onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
